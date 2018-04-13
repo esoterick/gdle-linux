b3bfe97c2eaa456692587083660adfdc0d76d633
Peanuts420 <asheronscall666@gmail.com>
2018-04-13 14:54:56 -0400

    Added switch case for augs

--- Source/AugmentationDevice.cpp
+++ Source/AugmentationDevice.cpp
@@ -22,42 +22,46 @@ int CAugmentationDeviceWeenie::Use(CPlayerWeenie *player)
 
 	long long augCost = InqInt64Quality(AUGMENTATION_COST_INT64, 0);
 	long long unassignedXP = player->InqInt64Quality(AVAILABLE_EXPERIENCE_INT64, 0);
-	int augJackOfTrades = player->InqIntQuality(AUGMENTATION_JACK_OF_ALL_TRADES_INT, 0);
-	
+	int aug = InqIntQuality(AUGMENTATION_STAT_INT, 0);
 
-	if (augJackOfTrades == 1)
+	switch (aug)
 	{
-		player->SendText("This augmentation is already active.", LTT_DEFAULT);
-		player->NotifyUseDone(WERROR_NONE);
-		return WERROR_NONE;
-	}
-
-	if (augJackOfTrades == 0)
+	case 40:
 	{
-		if (unassignedXP >= augCost)
+		int augJackOfTrades = player->InqIntQuality(AUGMENTATION_JACK_OF_ALL_TRADES_INT, 0);
+		if (augJackOfTrades == 1)
 		{
-			player->m_Qualities.SetInt(AUGMENTATION_JACK_OF_ALL_TRADES_INT, 1);
-			DecrementStackOrStructureNum();
-			player->m_Qualities.SetInt64(AVAILABLE_EXPERIENCE_INT64, unassignedXP - augCost);
-			player->NotifyInt64StatUpdated(AVAILABLE_EXPERIENCE_INT64);
-			//player->EmitSound(Sound_SkillDownVoid, 1.0, true);
-			player->EmitEffect(159, 1.0f);
-			player->SendText("Congratulations! You have succeeded in acquiring the Jack of All Trades augmentation.", LTT_DEFAULT);
-			//Cycle through the skills and notify the updated values
-			if (m_Qualities._skillStatsTable)
-			{
-				for (PackableHashTableWithJson<STypeSkill, Skill>::iterator entry = m_Qualities._skillStatsTable->begin(); entry != m_Qualities._skillStatsTable->end(); entry++)
-				{
-					STypeSkill skill = entry->first;
-					player->NotifySkillStatUpdated(skill);
-				}
-			}
-			
+			player->SendText("This augmentation is already active.", LTT_DEFAULT);
+			player->NotifyUseDone(WERROR_NONE);
+			return WERROR_NONE;
 		}
-		else
-			player->SendText("You do not have enough experience to use this augmentation gem.", LTT_DEFAULT);
+		if (augJackOfTrades == 0)
+		{
+			if (unassignedXP >= augCost)
+			{
+				player->m_Qualities.SetInt(AUGMENTATION_JACK_OF_ALL_TRADES_INT, 1);
+				DecrementStackOrStructureNum();
+				player->m_Qualities.SetInt64(AVAILABLE_EXPERIENCE_INT64, unassignedXP - augCost);
+				player->NotifyInt64StatUpdated(AVAILABLE_EXPERIENCE_INT64);
+				player->EmitEffect(159, 1.0f);
+				player->SendText("Congratulations! You have succeeded in acquiring the Jack of All Trades augmentation.", LTT_DEFAULT);
+				//Cycle through the skills and notify the updated values
+				if (m_Qualities._skillStatsTable)
+				{
+					for (PackableHashTableWithJson<STypeSkill, Skill>::iterator entry = m_Qualities._skillStatsTable->begin(); entry != m_Qualities._skillStatsTable->end(); entry++)
+					{
+						STypeSkill skill = entry->first;
+						player->NotifySkillStatUpdated(skill);
+					}
+				}
+
+			}
+			else
+				player->SendText("You do not have enough experience to use this augmentation gem.", LTT_DEFAULT);
+		}
 	}
-		
+	}
+
 	player->NotifyUseDone(WERROR_NONE);
 	return WERROR_NONE;
 }
