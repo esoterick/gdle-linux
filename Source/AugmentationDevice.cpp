a50017749e7277d3b253f8aa6da3e4469eced723
Peanuts420 <asheronscall666@gmail.com>
2018-04-13 16:45:28 -0400

    - Added support for Skilled Melee / Missile / Magic augs    + 10 base skills to weapon skills

--- Source/AugmentationDevice.cpp
+++ Source/AugmentationDevice.cpp
@@ -24,11 +24,115 @@ int CAugmentationDeviceWeenie::Use(CPlayerWeenie *player)
 	long long unassignedXP = player->InqInt64Quality(AVAILABLE_EXPERIENCE_INT64, 0);
 	int aug = InqIntQuality(AUGMENTATION_STAT_INT, 0);
 	int augJackOfTrades = player->InqIntQuality(AUGMENTATION_JACK_OF_ALL_TRADES_INT, 0);
+	int augSkilledMelee = player->InqIntQuality(AUGMENTATION_SKILLED_MELEE_INT, 0);
+	int augSkilledMissile = player->InqIntQuality(AUGMENTATION_SKILLED_MISSILE_INT, 0);
+	int augSkilledMagic = player->InqIntQuality(AUGMENTATION_SKILLED_MAGIC_INT, 0);
 
 	switch (aug)
 	{
-	case 40:
+	case 35:
+		if (augSkilledMelee == 1)
+		{
+			player->SendText("This augmentation is already active.", LTT_DEFAULT);
+			break;
+		}
+		if (augSkilledMelee == 0)
+		{
+			if (unassignedXP >= augCost)
+			{
+				player->m_Qualities.SetInt(AUGMENTATION_SKILLED_MELEE_INT, 1);
+				DecrementStackOrStructureNum();
+				player->m_Qualities.SetInt64(AVAILABLE_EXPERIENCE_INT64, unassignedXP - augCost);
+				player->NotifyInt64StatUpdated(AVAILABLE_EXPERIENCE_INT64);
+				player->EmitEffect(159, 1.0f);
+				player->SendText("Congratulations! You have succeeded in acquiring the Master of The Steel Circle augmentation.", LTT_DEFAULT);
+				//Need to cycle through the skills and notify the updated values here. Previous code was no good
 
+				//for (PackableHashTableWithJson<STypeSkill, Skill>::iterator entry = player->m_Qualities._skillStatsTable->begin(); entry != player->m_Qualities._skillStatsTable->end(); entry++)
+				//{
+				//DWORD val = 5;
+				//DWORD &valptr = val;
+				//STypeSkill skill = entry->first;
+				//player->InqSkill(skill, valptr, false);
+				//player->NotifySkillStatUpdated(skill);
+
+				//}
+
+				break;
+			}
+			else
+				player->SendText("You do not have enough experience to use this augmentation gem.", LTT_DEFAULT);
+			break;
+		}
+	case 36:
+		if (augSkilledMissile == 1)
+		{
+			player->SendText("This augmentation is already active.", LTT_DEFAULT);
+			break;
+		}
+		if (augSkilledMissile == 0)
+		{
+			if (unassignedXP >= augCost)
+			{
+				player->m_Qualities.SetInt(AUGMENTATION_SKILLED_MISSILE_INT, 1);
+				DecrementStackOrStructureNum();
+				player->m_Qualities.SetInt64(AVAILABLE_EXPERIENCE_INT64, unassignedXP - augCost);
+				player->NotifyInt64StatUpdated(AVAILABLE_EXPERIENCE_INT64);
+				player->EmitEffect(159, 1.0f);
+				player->SendText("Congratulations! You have succeeded in acquiring the Master of the Focused Eye augmentation.", LTT_DEFAULT);
+				//Need to cycle through the skills and notify the updated values here. Previous code was no good
+
+				//for (PackableHashTableWithJson<STypeSkill, Skill>::iterator entry = player->m_Qualities._skillStatsTable->begin(); entry != player->m_Qualities._skillStatsTable->end(); entry++)
+				//{
+				//DWORD val = 5;
+				//DWORD &valptr = val;
+				//STypeSkill skill = entry->first;
+				//player->InqSkill(skill, valptr, false);
+				//player->NotifySkillStatUpdated(skill);
+
+				//}
+
+				break;
+			}
+			else
+				player->SendText("You do not have enough experience to use this augmentation gem.", LTT_DEFAULT);
+			break;
+		}
+	case 37:
+		if (augSkilledMagic == 1)
+		{
+			player->SendText("This augmentation is already active.", LTT_DEFAULT);
+			break;
+		}
+		if (augSkilledMagic == 0)
+		{
+			if (unassignedXP >= augCost)
+			{
+				player->m_Qualities.SetInt(AUGMENTATION_SKILLED_MAGIC_INT, 1);
+				DecrementStackOrStructureNum();
+				player->m_Qualities.SetInt64(AVAILABLE_EXPERIENCE_INT64, unassignedXP - augCost);
+				player->NotifyInt64StatUpdated(AVAILABLE_EXPERIENCE_INT64);
+				player->EmitEffect(159, 1.0f);
+				player->SendText("Congratulations! You have succeeded in acquiring the Master of The Five Fold Path augmentation.", LTT_DEFAULT);
+				//Need to cycle through the skills and notify the updated values here. Previous code was no good
+
+				//for (PackableHashTableWithJson<STypeSkill, Skill>::iterator entry = player->m_Qualities._skillStatsTable->begin(); entry != player->m_Qualities._skillStatsTable->end(); entry++)
+				//{
+				//DWORD val = 5;
+				//DWORD &valptr = val;
+				//STypeSkill skill = entry->first;
+				//player->InqSkill(skill, valptr, false);
+				//player->NotifySkillStatUpdated(skill);
+
+				//}
+
+				break;
+			}
+			else
+				player->SendText("You do not have enough experience to use this augmentation gem.", LTT_DEFAULT);
+			break;
+		}
+	case 40:
 		if (augJackOfTrades == 1)
 		{
 			player->SendText("This augmentation is already active.", LTT_DEFAULT);
@@ -61,11 +165,9 @@ int CAugmentationDeviceWeenie::Use(CPlayerWeenie *player)
 			else
 				player->SendText("You do not have enough experience to use this augmentation gem.", LTT_DEFAULT);
 			break;
-
-
 		}
 	default:
-		player->SendText("You do not have enough experience to use this augmentation gem.", LTT_DEFAULT);
+		player->SendText("This Augmentation is not supported at this time!", LTT_DEFAULT);
 		break;
 	}
 
