ad391703a9ee126be91720e8e45a848320288872
Simon Jarrett <simonjjarrett@gmail.com>
2018-05-09 00:07:17 +0100

    Adjusted the damage bonus for war magic spells to significantly improve level 8 spells

--- Source/PhatSDK/Support/CombatFormulas.cpp
+++ Source/PhatSDK/Support/CombatFormulas.cpp
@@ -115,7 +115,13 @@ void CalculateSkillDamageBonus(DamageEventData *dmgEvent, SpellCastData *spellDa
 			{
 				float minDamage = (float)meta->_baseIntensity;
 
-				float skillDamageMod = ((int)spellData->current_skill - (spellData->spell->_power + 50)) / 250.0; //made up formula.
+				float spellPower = spellData->spell->_power;
+				if (spellPower == 400)
+				{
+					spellPower -= 50; // Adjust for level 8s
+				}
+
+				float skillDamageMod = ((int)spellData->current_skill - spellPower) / 1000.0; //better made up formula.
 				if (skillDamageMod > 0)
 					dmgEvent->skillDamageBonus = minDamage * skillDamageMod;
 			}
