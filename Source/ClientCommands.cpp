661a6dee5d920d625faed2a0bde09a38b3420ac6
Michael Rath <mikerath@gmail.com>
2018-06-24 22:29:16 -0700

    Check to verify arguements sent to /hover

--- Source/ClientCommands.cpp
+++ Source/ClientCommands.cpp
@@ -4550,6 +4550,12 @@ CLIENT_COMMAND(hover, "<on / off>", "Turns hovering on or off.", BASIC_ACCESS)
 		return false;
 	}
 
+	if (argc < 1)
+	{
+		pPlayer->SendText("Usage: /hover on | off", LTT_DEFAULT);
+		return false;
+	}
+
 	if (!_stricmp(argv[0], "on") || !_stricmp(argv[0], "1"))
 	{
 
