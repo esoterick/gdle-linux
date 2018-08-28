1ecdcfba75829d3da57fd66cd14a690ba3665b3a
Michael Rath <mikerath@gmail.com>
2018-08-28 10:28:53 -0700

    Remove Alleg bool upon breaking allegiance to start clean on new patron

--- Source/AllegianceManager.cpp
+++ Source/AllegianceManager.cpp
@@ -804,6 +804,9 @@ void AllegianceManager::BreakAllAllegiance(DWORD char_id)
 			RemoveAllegianceNode(selfTreeNode);
 		}
 	}
+
+	CPlayerWeenie* source = g_pWorld->FindPlayer(char_id);
+	source->m_Qualities.RemoveBool(EXISTED_BEFORE_ALLEGIANCE_XP_CHANGES_BOOL);
 }
 
 void AllegianceManager::HandleAllegiancePassup(DWORD source_id, long long amount, bool direct)
@@ -831,28 +834,11 @@ void AllegianceManager::HandleAllegiancePassup(DWORD source_id, long long amount
 		{
 			source->m_Qualities.SetBool(EXISTED_BEFORE_ALLEGIANCE_XP_CHANGES_BOOL, true);
 		}
-		else if (CWeenieObject* patronWeenie = g_pWorld->FindPlayer(node->_patronID))  // patron is online to get their level
+		else if (node->_level <= patron->_level)
 		{
-			if (source->m_Qualities.GetInt(LEVEL_INT, 2) <= patronWeenie->m_Qualities.GetInt(LEVEL_INT, 1))
-				source->m_Qualities.SetBool(EXISTED_BEFORE_ALLEGIANCE_XP_CHANGES_BOOL, true);
-			else
-				return;
+			source->m_Qualities.SetBool(EXISTED_BEFORE_ALLEGIANCE_XP_CHANGES_BOOL, true);
 		}
-		else if (CWeenieObject* patronWeenie = CWeenieObject::Load(node->_patronID)) // temporarily load the patron so we can compare levels
-		{
-			if (source->m_Qualities.GetInt(LEVEL_INT, 2) <= patronWeenie->m_Qualities.GetInt(LEVEL_INT, 1))
-			{
-				delete patronWeenie;
-				source->m_Qualities.SetBool(EXISTED_BEFORE_ALLEGIANCE_XP_CHANGES_BOOL, true);
-			}
-			else 
-			{
-				delete patronWeenie;
-				return;
-			}
-				
-		}
-		else
+		else 
 			return; // bool remains false, don't passup
 	}
 
