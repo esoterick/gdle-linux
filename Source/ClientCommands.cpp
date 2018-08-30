c21264bb2cfbd0b1ecceebb573fca7fe022dbd3d
Michael Rath <mikerath@gmail.com>
2018-08-29 20:51:49 -0700

    Return of Allegiance Hometown recall Allegdump admin command Allegiance hometown bind saves immediately vs up to 5 min away

--- Source/ClientCommands.cpp
+++ Source/ClientCommands.cpp
@@ -223,6 +223,51 @@ CLIENT_COMMAND(spawndoor, "", "Spawns a door at your location.", ADMIN_ACCESS)
 }
 #endif
 
+CLIENT_COMMAND(allegdump, "", "Prints out the allegiance hierarchy info on the last target you assessed.", ADMIN_ACCESS)
+{
+	if (argc < 1)
+		return true;
+
+	if (pPlayer->m_LastAssessed)
+	{
+		CWeenieObject *target = g_pWorld->FindObject(pPlayer->m_LastAssessed);
+		if (target)
+		{
+			AllegianceTreeNode *node = g_pAllegianceManager->GetTreeNode(target->GetID());
+			if (node)
+			{
+				AllegianceInfo *info = g_pAllegianceManager->GetInfo(node->_monarchID);
+				if (info)
+				{
+					AllegianceTreeNode *monarch = g_pAllegianceManager->GetTreeNode(node->_monarchID);
+					// Monarch name
+					pPlayer->SendText(csprintf("Monarch: %s", monarch->_charName), LTT_DEFAULT);
+					
+					AllegianceTreeNode *patron = g_pAllegianceManager->GetTreeNode(node->_patronID);
+					// Patron name
+					pPlayer->SendText(csprintf("Patron: %s", patron->_charName), LTT_DEFAULT);
+
+					// Allegiance name
+					pPlayer->SendText(csprintf("Allegiance Name: %s", info->_info.m_AllegianceName), LTT_DEFAULT);
+					// Number in allegiance
+					pPlayer->SendText(csprintf("Number in Allegiance: %i", monarch->_numFollowers), LTT_DEFAULT);
+					// Number of Vassels
+					pPlayer->SendText(csprintf("Number in Vassels: %i", node->_numFollowers), LTT_DEFAULT);
+					// Bind point
+					Vector *f = info->_info.m_BindPoint.get_origin();
+					float heading = info->_info.m_BindPoint.heading(info->_info.m_BindPoint);
+					pPlayer->SendText(csprintf("Allegiance Hometown: 0x%08x [ X:%.2f Y:%.2f Z:%.2f ] w:%.2f x:0.0 y:0.0 z:0.0", info->_info.m_BindPoint.objcell_id, 
+						f->x, f->y, f->z, heading), LTT_DEFAULT);
+				}
+			}
+			
+			
+		}
+	}
+
+	return false;
+}
+
 CLIENT_COMMAND(global, "<text> [color=1]", "Displays text globally.", ADMIN_ACCESS)
 {
 	if (argc < 1)
--- Source/ClientEvents.cpp
+++ Source/ClientEvents.cpp
@@ -1264,7 +1264,9 @@ void CClientEvents::AllegianceHometownRecall()
 	if (allegianceInfo && allegianceInfo->_info.m_BindPoint.objcell_id)
 	{
 		if (!m_pPlayer->IsBusyOrInAction())
+		{
 			m_pPlayer->ExecuteUseEvent(new CAllegianceHometownRecallUseEvent());
+		}
 	}
 	else
 		m_pPlayer->NotifyWeenieError(WERROR_ALLEGIANCE_HOMETOWN_NOT_SET);
@@ -3309,8 +3311,9 @@ void CClientEvents::ProcessEvent(BinaryReader *pReader)
 		}
 		case RECALL_ALLEGIANCE_HOMETOWN: //Allegiance_RecallAllegianceHometown (bindstone)
 		{
-			MAllegianceRecallHometown_02AB msg(m_pPlayer);
-			msg.Parse(pReader);
+			AllegianceHometownRecall();
+			/*MAllegianceRecallHometown_02AB msg(m_pPlayer);
+			msg.Parse(pReader);*/
 			break;
 		}
 		case FINISH_BARBER:
