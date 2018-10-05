f927c7c4e916852fdd65c521bd0495dd7074d80b
LikeableLime <LikeableLime@gmail.com>
2018-10-04 21:32:10 -0500

    Changes to Database2.cpp to support new stored procedures.

--- Source/Database2.cpp
+++ Source/Database2.cpp
@@ -433,7 +433,7 @@ bool CMYSQLSaveWeenieQuery::PerformQuery(MYSQL *c)
 	}
 
 	char query_string[512];
-	sprintf(query_string, "REPLACE INTO weenies (id, top_level_object_id, block_id, data) VALUES (%u, %u, %u, ?)", _weenie_id, _top_level_object_id, _block_id);
+	sprintf(query_string, "CALL blob_update_weenie(%u, %u, %u, ?)", _weenie_id, _top_level_object_id, _block_id);
 
 	if (mysql_stmt_prepare(statement, query_string, (unsigned long)strlen(query_string)))
 	{
@@ -509,7 +509,7 @@ bool CMYSQLSaveHouseQuery::PerformQuery(MYSQL *c)
 	}
 
 	char query_string[512];
-	sprintf(query_string, "REPLACE INTO houses (house_id, data) VALUES (%u, ?)", _house_id);
+	sprintf(query_string, "CALL blob_update_house(%u, ?)", _house_id);
 
 	if (mysql_stmt_prepare(statement, query_string, (unsigned long)strlen(query_string)))
 	{
