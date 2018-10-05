a5331accfd6199612a4d7772a09e948672da5452
Adam Van Campen <adamjvc@gmail.com>
2018-10-04 23:06:09 -0400

    Resolving Merge Conflicts

--- Source/EmoteManager.cpp
+++ Source/EmoteManager.cpp
@@ -1303,19 +1303,18 @@ void EmoteManager::ExecuteEmote(const Emote &emote, DWORD target_id)
 	}
 
 	case SetQuestCompletions_EmoteType:
-
+	{
 		if (!_weenie->m_Qualities._emote_table)
 			break;
-	{
+
 		CWeenieObject *target = g_pWorld->FindObject(target_id);
 
 		if (target)
 		{
 			target->SetQuestCompletions(emote.msg.c_str(), emote.amount);
 		}
-
+		break;
 	}
-	break;
 
 	case Generate_EmoteType: //type:72 adds from generator table attached to creature weenie. Sets init value of generator table and calls weenie factory to begin generation. Can use same emote with value of 0 in amount field to disable generator.
 	{
@@ -1667,9 +1666,60 @@ void EmoteManager::ExecuteEmote(const Emote &emote, DWORD target_id)
 		}
 		break;
 	}
+
+	case DeleteSelf_EmoteType:
+	{
+		if (!_weenie->m_Qualities._emote_table)
+			break;
+
+		_weenie->MarkForDestroy();
+		break;
+	}
+
+	case KillSelf_EmoteType:
+	{
+		if (!_weenie->m_Qualities._emote_table)
+			break;
+
+		CMonsterWeenie *monster = _weenie->AsMonster();
+		if (monster && !monster->IsDead() && !monster->IsInPortalSpace() && !monster->IsBusyOrInAction())
+		{
+			monster->SetHealth(0, true);
+			monster->OnDeath(monster->GetID());
+		}
+
+		break;
+
+	}
+
+	case SetBoolStat_EmoteType:
+	{
+		if (!_weenie->m_Qualities._emote_table)
+			break;
+
+		if (emote.extent == 2) //if extent is 2 then set bool on self.
+		{
+			_weenie->m_Qualities.SetBool((STypeBool)emote.stat, emote.amount);
+			_weenie->NotifyBoolStatUpdated((STypeBool)emote.stat, FALSE);
+		}
+
+		else
+		{
+			CWeenieObject *target = g_pWorld->FindObject(target_id);
+			if (target)
+			{
+				target->m_Qualities.SetBool((STypeBool)emote.stat, emote.amount);
+				target->NotifyBoolStatUpdated((STypeBool)emote.stat, FALSE);
+			}
+		}
+
+		break;
+	}
+
 	}
 	_weenie->m_Qualities.SetBool(EXECUTING_EMOTE, false);
 }
+
 
 bool EmoteManager::IsExecutingAlready()
 {
@@ -1692,9 +1742,14 @@ void EmoteManager::Tick()
 			break;
 
 		ExecuteEmote(i->_data, i->_target_id);
+
+		//Check if emote queue is empty due to KillSelf_emoteType
+		if (_emoteQueue.empty())
+			return;
+
 		i = _emoteQueue.erase(i);
 		if (i != _emoteQueue.end())
-			i->_executeTime = Timer::cur_time + i->_data.delay;		
+			i->_executeTime = Timer::cur_time + i->_data.delay;
 	}
 }
 
