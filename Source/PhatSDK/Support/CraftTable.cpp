ed56c8aecb55341e74f249feff14f41b36553b53
CodeMunkey <@codemunkey>
2018-10-14 21:27:32 -0500

    Working on correct export

--- Source/PhatSDK/Support/CraftTable.cpp
+++ Source/PhatSDK/Support/CraftTable.cpp
@@ -35,17 +35,23 @@ DEFINE_UNPACK_JSON(CCraftOperation)
 	_failureConsumeToolMessage = reader["FailureConsumeToolMessage"];
 
 	const json &reqs = reader["Requirements"];
-
-	for (const json &r : reqs)
+	if (reqs.is_array())
 	{
-		_requirements->UnPackJson(r);
+		// must have 3, null is valid
+		for (int i = 0; i < 3; i++)
+		{
+			_requirements[i].UnPackJson(reqs[i]);
+		}
 	}
 
 	const json &mods = reader["Mods"];
-
-	for (const json &m : mods)
+	if (mods.is_array())
 	{
-		_mods->UnPackJson(m);
+		// must have 8, null is valid
+		for (int i = 0; i < 8; i++)
+		{
+			_mods[i].UnPackJson(mods[i]);
+		}
 	}
 
 	_dataID = reader["DataID"];
@@ -125,7 +131,7 @@ DEFINE_PACK_JSON(CCraftOperation)
 	for (DWORD i = 0; i < 3; i++)
 	{
 		json req;
-		_requirements->PackJson(req);
+		_requirements[i].PackJson(req);
 		requirements.push_back(req);
 	}
 
@@ -136,7 +142,7 @@ DEFINE_PACK_JSON(CCraftOperation)
 	for (DWORD i = 0; i < 8; i++)
 	{
 		json mod;
-		_mods->PackJson(mod);
+		_mods[i].PackJson(mod);
 		mods.push_back(mod);
 	}
 
@@ -199,45 +205,100 @@ DEFINE_UNPACK(CraftRequirements)
 
 DEFINE_PACK_JSON(CraftRequirements)
 {
-	_intRequirement.PackJson(writer);
-	_didRequirement.PackJson(writer);
-	_iidRequirement.PackJson(writer);
-	_floatRequirement.PackJson(writer);
-	_stringRequirement.PackJson(writer);
-	_boolRequirement.PackJson(writer);
+	if (_intRequirement.size() > 0)
+	{
+		json ints;
+		_intRequirement.PackJson(ints);
+
+		writer["IntRequirements"] = ints;
+	}
+
+	if (_didRequirement.size() > 0)
+	{
+		json dids;
+		_didRequirement.PackJson(dids);
+
+		writer["DIDRequirements"] = dids;
+	}
+
+	if (_iidRequirement.size() > 0)
+	{
+		json iids;
+		_iidRequirement.PackJson(iids);
+
+		writer["IIDRequirements"] = iids;
+	}
+
+	if (_floatRequirement.size() > 0)
+	{
+		json floats;
+		_floatRequirement.PackJson(floats);
+
+		writer["FloatRequirements"] = floats;
+	}
+
+	if (_stringRequirement.size() > 0)
+	{
+		json strings;
+		_stringRequirement.PackJson(strings);
+
+		writer["StringRequirements"] = strings;
+	}
+
+	if (_boolRequirement.size() > 0)
+	{
+		json bools;
+		_boolRequirement.PackJson(bools);
+
+		writer["BoolRequirements"] = bools;
+	}
+
 }
 
 DEFINE_UNPACK_JSON(CraftRequirements)
 {
-	if (reader.find("IntRequirements") != reader.end())
+	if (reader.is_null())
+		return true;
+
+	json::const_iterator itr = reader.end();
+	json::const_iterator end = reader.end();
+
+	itr = reader.find("IntRequirements");
+	if (itr != end)
 	{
-		_intRequirement.UnPackJson(reader);
+		_intRequirement.UnPackJson(*itr);
 	}
 
-	if (reader.find("DIDRequirements") != reader.end())
+	itr = reader.find("DIDRequirements");
+	if (itr != end)
 	{
-		_didRequirement.UnPackJson(reader);
+		_didRequirement.UnPackJson(*itr);
 	}
 
-	if (reader.find("IIDRequirements") != reader.end())
+	itr = reader.find("IIDRequirements");
+	if (itr != end)
 	{
-		_iidRequirement.UnPackJson(reader);
+		_iidRequirement.UnPackJson(*itr);
 	}
 
-	if (reader.find("FloatRequirements") != reader.end())
+	itr = reader.find("FloatRequirements");
+	if (itr != end)
 	{
-		_floatRequirement.UnPackJson(reader);
+		_floatRequirement.UnPackJson(*itr);
 	}
 
-	if (reader.find("StringRequirements") != reader.end())
+	itr = reader.find("StringRequirements");
+	if (itr != end)
 	{
-		_stringRequirement.UnPackJson(reader);
+		_stringRequirement.UnPackJson(*itr);
 	}
 
-	if (reader.find("BoolRequirements") != reader.end())
+	itr = reader.find("BoolRequirements");
+	if (itr != end)
 	{
-		_boolRequirement.UnPackJson(reader);
+		_boolRequirement.UnPackJson(*itr);
 	}
+
 	return true; 
 }
 
@@ -288,17 +349,23 @@ DEFINE_UNPACK_JSON(JsonCraftOperation)
 	_failureConsumeToolMessage = reader["FailureConsumeToolMessage"];
 
 	const json &reqs = reader["Requirements"];
-
-	for (const json &r : reqs)
+	if (reqs.is_array())
 	{
-		_requirements->UnPackJson(r);
+		// must have 3, null is valid
+		for (int i = 0; i < 3; i++)
+		{
+			_requirements[i].UnPackJson(reqs[i]);
+		}
 	}
 
 	const json &mods = reader["Mods"];
-
-	for (const json &m : mods)
+	if (mods.is_array())
 	{
-		_mods->UnPackJson(m);
+		// must have 8, null is valid
+		for (int i = 0; i < 8; i++)
+		{
+			_mods[i].UnPackJson(mods[i]);
+		}
 	}
 
 	_dataID = reader["DataID"];
@@ -409,104 +476,94 @@ bool CraftMods::UnPack(BinaryReader * pReader)
 
 void CraftMods::PackJson(json & writer)
 {
+	bool isnull = true;
+
 	if (_intMod.size() > 0)
 	{
 		json intMods;
-
-		for (DWORD i = 0; i < 3; i++)
-		{
-			json req;
-			_intMod.PackJson(req);
-			intMods.push_back(req);
-		}
+		_intMod.PackJson(intMods);
 
 		writer["IntRequirements"] = intMods;
+		
+		isnull = false;
 	}
 
 	if (_didMod.size() > 0)
 	{
 		json didMod;
-
-		for (DWORD i = 0; i < 3; i++)
-		{
-			json req;
-			_didMod.PackJson(req);
-			didMod.push_back(req);
-		}
+		_didMod.PackJson(didMod);
 
 		writer["DIDRequirements"] = didMod;
+
+		isnull = false;
 	}
 
 	if (_iidMod.size() > 0)
 	{
 		json iidMod;
-
-		for (DWORD i = 0; i < 3; i++)
-		{
-			json req;
-			_iidMod.PackJson(req);
-			iidMod.push_back(req);
-		}
+		_iidMod.PackJson(iidMod);
 
 		writer["IIDRequirements"] = iidMod;
+
+		isnull = false;
 	}
 
 	if (_floatMod.size() > 0)
 	{
 		json floatMod;
-
-		for (DWORD i = 0; i < 3; i++)
-		{
-			json req;
-			_floatMod.PackJson(req);
-			floatMod.push_back(req);
-		}
+		_floatMod.PackJson(floatMod);
 
 		writer["FloatRequirements"] = floatMod;
+
+		isnull = false;
 	}
 
 	if (_stringMod.size() > 0)
 	{
 		json stringMod;
-
-		for (DWORD i = 0; i < 3; i++)
-		{
-			json req;
-			_stringMod.PackJson(req);
-			stringMod.push_back(req);
-		}
+		_stringMod.PackJson(stringMod);
 
 		writer["StringRequirements"] = stringMod;
+
+		isnull = false;
 	}
 
 	if (_boolMod.size() > 0)
 	{
 		json boolMod;
-
-		for (DWORD i = 0; i < 3; i++)
-		{
-			json req;
-			_boolMod.PackJson(req);
-			boolMod.push_back(req);
-		}
+		_boolMod.PackJson(boolMod);
 
 		writer["BoolRequirements"] = boolMod;
+
+		isnull = false;
 	}
 
-	writer["ModifyHealth"] = _ModifyHealth;
-	writer["ModifyStamina"] = _ModifyStamina;
-	writer["ModifyMana"] = _ModifyMana;
-	writer["RequiresHealth"] = _RequiresHealth;
-	writer["RequiresStamina"] = _RequiresStamina;
-	writer["RequiresMana"] = _RequiresMana;
-	writer["Unknown7"] = _unknown7;
-	writer["ModificationScriptId"] = _modificationScriptId;
-	writer["Unknown9"] = _unknown9;
-	writer["Unknown10"] = _unknown10;
+	isnull &= _ModifyHealth == 0 && _ModifyStamina == 0 &&
+		_ModifyMana == 0 && _RequiresHealth == 0 &&
+		_RequiresStamina == 0 && _RequiresMana == 0 &&
+		_unknown7 == false && _modificationScriptId == 0 &&
+		_unknown9 == 0 && _unknown10 == 0;
+
+	if (!isnull)
+	{
+		writer["ModifyHealth"] = _ModifyHealth;
+		writer["ModifyStamina"] = _ModifyStamina;
+		writer["ModifyMana"] = _ModifyMana;
+		writer["RequiresHealth"] = _RequiresHealth;
+		writer["RequiresStamina"] = _RequiresStamina;
+		writer["RequiresMana"] = _RequiresMana;
+		writer["Unknown7"] = _unknown7;
+		writer["ModificationScriptId"] = _modificationScriptId;
+		writer["Unknown9"] = _unknown9;
+		writer["Unknown10"] = _unknown10;
+	}
 }
 
 bool CraftMods::UnPackJson(const json & reader)
 {
+	if (reader.is_null())
+		return true;
+
 	json::const_iterator itr = reader.end();
 	json::const_iterator end = reader.end();
 
