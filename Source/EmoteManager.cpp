6d7830ca573d58a89a54d6d16503b97665294d8f
Adam Van Campen <adamjvc@gmail.com>
2018-10-20 16:09:06 -0400

    Adding InqPackSpace_EmoteType

--- Source/EmoteManager.cpp
+++ Source/EmoteManager.cpp
@@ -1737,7 +1737,23 @@ void EmoteManager::ExecuteEmote(const Emote &emote, DWORD target_id)
 
 		break;
 	}
+	case InqPackSpace_EmoteType:
+	{
+		if (!_weenie->m_Qualities._emote_table)
+			break;
 
+		CPlayerWeenie *target = g_pWorld->FindPlayer(target_id);
+		if (target)
+		{
+			bool success = false;
+			if (target->Container_GetNumFreeMainPackSlots() >= emote.amount)
+				success = true;
+
+			ChanceExecuteEmoteSet(success ? TestSuccess_EmoteCategory : TestFailure_EmoteCategory, emote.msg, target_id);
+		}
+
+		break;
+	}
 	}
 	_weenie->m_Qualities.SetBool(EXECUTING_EMOTE, false);
 }
