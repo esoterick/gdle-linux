60f56cad690348c3c18a870812e180ca6b5a5b41
Zarto <adamjvc@gmail.com>
2018-11-04 10:56:07 -0500

    Added full list of restricted landblocks from wiki.

--- Source/Player.cpp
+++ Source/Player.cpp
@@ -3266,8 +3266,10 @@ void CPlayerWeenie::SetLoginPlayerQualities()
 	/*
 	Below are a list of landblocks restricted from login. If a player's location position matches the listed landblocks their login position will be changed (one time) to their sanctuary position.
 	THIS CODED LIST IS TEMPORARY - This should be moved to either a config loaded option or a JSON loaded format.
+	===========LIST==========
+	Viamontian Garrison, Augmentation Realm Main Level, Tanada House of Pancakes (Seasonal), Augmentation Realm Upper Level, Augmentation Realm Lower Level, Derethian Combat Arena, Derethian Combat Arena, Blighted Putrid Moarsman Tunnels, Jester's Prison, Mhoire Armory, Mountain Cavern, East Fork Dam Hive, Mount Elyrii Hive, Oubliette of Mhoire Castle, Tainted Grotto, Greater Battle Dungeon, Hoshino Tower, Thug Hideout, Night Club (Seasonal Anniversary), Frozen Wight Lair, Northwatch Castle Black Market, Aerfalle's Sanctum, Freebooter Keep Black Market
 	*/
-	std::set<DWORD> NoLogLandBlocks{ 7143424, 8192000, 5636096 }; //augmentation realm upper, augmentation realm lower, augmentation realm main
+	std::set<DWORD> NoLogLandBlocks{ 131072, 5636096, 6225920, 7143424, 8192000, 11206656, 11272192, 12779520, 14090240, 15335424, 22872064, 41877504, 61276160, 1466171392, 1665925120, 1698955264, 1716584448, 2114191360, 2315517952, 2332295168, 2665807872, 3052404736, 4180606976 };
 
 	Position m_initLocPosition;
 	if (m_Qualities.InqPosition(LOCATION_POSITION, m_initLocPosition) && m_initLocPosition.objcell_id)
