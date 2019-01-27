b8ad5c36ddb1a5d5e8fcdf7f2f2ff04e361dd36b
Robert Lambert <rlambert85@gmail.com>
2019-01-27 08:49:31 +0000

    updating documentation a little bit

--- Bin/sqldumps/blob_update_house.sql
+++ Bin/sqldumps/blob_update_house.sql
@@ -1,3 +1,4 @@
+DELIMITER //
 CREATE DEFINER=`root`@`localhost` PROCEDURE `blob_update_house`(
     IN houseId INT(11),
     IN houseData LONGBLOB
@@ -8,5 +9,5 @@ INSERT INTO houses (house_id, DATA)
     VALUES (houseId, houseData)
     ON DUPLICATE KEY UPDATE
         DATA=houseData;
-
-END
+END //
+DELIMITER ;
