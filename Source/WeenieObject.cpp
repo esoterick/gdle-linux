a5ca8b24381d44556a50a676fba50681bea0bfb8
LikeableLime <likeablelime@gmail.com>
2019-02-10 01:05:52 +0000

    Reduce teleport timestamp float to 5 secs.

--- Source/WeenieObject.cpp
+++ Source/WeenieObject.cpp
@@ -6682,7 +6682,7 @@ void CWeenieObject::OnTeleported()
 		AsPlayer()->CancelLifestoneProtection(); //terminate lifestone protection on teleport (catch all for portal use and /recalls. Casted portals are captured in SpellCastingManager.cpp)
 
 	if (AsPlayer())
-		m_Qualities.SetFloat(LAST_TELEPORT_START_TIMESTAMP_FLOAT, Timer::cur_time + 7.0);
+		m_Qualities.SetFloat(LAST_TELEPORT_START_TIMESTAMP_FLOAT, Timer::cur_time + 5.0);
 }
 
 void CWeenieObject::Movement_Teleport(const Position &position, bool bWasDeath)
